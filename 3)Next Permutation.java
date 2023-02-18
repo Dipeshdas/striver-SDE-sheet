5519fc35811fca4a37c172a2c1c9c5826f8653bf
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-02-18 12:18:04 +0530

    Update 3)Next Permutation.java

--- 3)Next Permutation.java
+++ 3)Next Permutation.java
@@ -1,3 +1,5 @@
+
+//LEETCODE SOLUTION
 class Solution {
     public void nextPermutation(int[] nums) {
         if(nums==null || nums.length<=1) return;
@@ -28,3 +30,34 @@ class Solution {
     }
 
 }
+
+//CODE STUDIO SOLUTION
+
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution 
+{
+	public static ArrayList<Integer> nextPermutation(ArrayList<Integer> permutation) 
+	{
+		if(permutation==null || permutation.size()<=1) return permutation;
+
+		int i=permutation.size()-2;
+		while(i>=0 && permutation.get(i)>=permutation.get(i+1)) i--;
+		if(i>=0){
+			int j=permutation.size()-1;
+			while(permutation.get(j)<=permutation.get(i)) j--;
+			Collections.swap(permutation,i,j);
+		}
+		reverse(permutation,i+1,permutation.size()-1);
+		return permutation;
+	}
+	public static void reverse (ArrayList<Integer> permutation,int i,int j){
+        while(i<j){
+            Collections.swap(permutation,i++,j--);
+        }
+    }
+}
+
