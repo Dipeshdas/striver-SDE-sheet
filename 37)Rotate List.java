2238c0c60710cb358051b5e0253847ec92d63f1f
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-03-06 10:16:27 +0530

    Update 37)Rotate List.java

--- 37)Rotate List.java
+++ 37)Rotate List.java
@@ -33,3 +33,47 @@ class Solution {
         return head;
     }
 }
+
+//CODE STUDIO
+import java.util.* ;
+import java.io.*; 
+import java.util.* ;
+import java.io.*; 
+/***********************************************
+    Following is the class structure of the Node class:
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int x) {
+            this.data = x;
+            this.next = null;
+        }
+    }
+
+************************************************/
+
+public class Solution {
+    public static Node rotate(Node head, int k) {
+        if(head ==null || head.next==null|| k==0) return head;
+
+        Node cur=head;
+        int len=1;
+        while(cur.next!=null){
+            cur=cur.next;
+            len++;
+        }
+
+        cur.next=head;
+        k=k%len;
+        k=len-k;
+        while(k!=0){
+            cur=cur.next;
+            k--;
+        }
+        head=cur.next;
+        cur.next=null;
+        return head;
+    }
+}
