79188c17ccf27d337ce0a93ee8be83c5f78e1488
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-03-11 10:11:28 +0530

    Create 46)Fractional Knapsack.java

---
+++ 46)Fractional Knapsack.java
@@ -0,0 +1,131 @@
+//GFG
+
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+
+class GfG {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int w = Integer.parseInt(inputLine[1]);
+            Item[] arr = new Item[n];
+            inputLine = br.readLine().trim().split(" ");
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Item(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            System.out.println(String.format("%.2f", new Solution().fractionalKnapsack(w, arr, n)));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+*/
+
+class ItemComparator implements Comparator<Item>{
+    public int compare(Item a, Item b) 
+    {
+        double r1 = (double)(a.value) / (double)(a.weight); 
+        double r2 = (double)(b.value) / (double)(b.weight); 
+        if(r1 < r2) return 1; 
+        else if(r1 > r2) return -1; 
+        else return 0; 
+    }
+}
+
+class Solution
+{
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n) 
+    {
+        Arrays.sort(arr,new ItemComparator());
+        int curWeight=0;
+        double ans=0.0;
+        for(int i=0;i<n;i++){
+            if(arr[i].weight+curWeight<=W){
+                ans+=arr[i].value;
+                curWeight+=arr[i].weight;
+            }
+            else{
+                int remain=W-curWeight;
+                ans+=((double)arr[i].value/(double)arr[i].weight)*remain;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+
+//CODE STUDIO
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Pair class:
+
+        class Pair
+        {
+        	int weight;
+	        int value;
+	        Pair(int weight, int value)
+	        {
+		        this.weight = weight;
+		        this.value = value;
+	        }
+	        
+        }
+        
+*****************************************************************/
+
+class ItemComparator implements Comparator<Pair>{
+    public int compare(Pair a, Pair b) 
+    {
+        double r1 = (double)(a.value) / (double)(a.weight); 
+        double r2 = (double)(b.value) / (double)(b.weight); 
+        if(r1 < r2) return 1; 
+        else if(r1 > r2) return -1; 
+        else return 0; 
+    }
+}
+
+public class Solution {
+    public static double maximumValue(Pair[] items, int n, int w) {
+        Arrays.sort(items,new ItemComparator());
+        int curWeight=0;
+        double ans=0.0;
+        for(int i=0;i<n;i++){
+            if(items[i].weight+curWeight<=w){
+                ans+=items[i].value;
+                curWeight+=items[i].weight;
+            }
+            else{
+                int remain=w-curWeight;
+                ans+=((double)items[i].value/(double)items[i].weight)*remain;
+                break;
+            }
+        }
+        return ans;
+    }
+}
