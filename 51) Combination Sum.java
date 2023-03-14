0e678c138a2702e15e983d87f6a123e762507df2
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-03-14 10:11:30 +0530

    Update 51) Combination Sum.java

--- 51) Combination Sum.java
+++ 51) Combination Sum.java
@@ -21,3 +21,34 @@ class Solution {
 
     }
 }
+
+//CODE STUDIO
+
+import java.util.*;
+public class Solution 
+{
+    public static ArrayList<ArrayList<Integer>> findSubsetsThatSumToK(ArrayList<Integer> arr, int n, int k) 
+	{
+        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+        solve(0,arr,k,ans,new ArrayList<>());
+        return ans;
+    }
+
+    public static void solve(int ind,ArrayList<Integer> arr,int target,ArrayList<ArrayList<Integer>>ans,ArrayList<Integer>temp){
+        if(ind==arr.size()){
+            if(target==0 && temp.size()!=0){
+                ans.add(temp);
+            }
+            return;
+        }
+        solve(ind+1,arr,target,ans,temp);//for not pick
+
+        ArrayList<Integer> temp2 = new ArrayList<>(temp);
+        temp2.add(arr.get(ind));
+
+        solve(ind+1,arr,target-arr.get(ind),ans,temp2);//for pick
+        
+        
+
+    }
+}
