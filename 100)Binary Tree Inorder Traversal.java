956f70bb22ac9a77d659efe7f2522a99f94b7d2a
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-03-30 22:27:43 +0530

    Update 100)Binary Tree Inorder Traversal.java

--- 100)Binary Tree Inorder Traversal.java
+++ 100)Binary Tree Inorder Traversal.java
@@ -33,22 +33,56 @@ class Solution {
 
 
     //Iterative Solution
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer>l=new ArrayList<>();
+    //     Stack<TreeNode>st=new Stack<>();
+    //     TreeNode temp=root;
+    //     while(true){
+    //         if(temp!=null){
+    //             st.push(temp);
+    //             temp=temp.left;
+    //         }
+    //         else{
+    //             if(st.isEmpty()) break;
+    //             temp=st.pop();
+    //             l.add(temp.val);
+    //             temp=temp.right;
+    //         }
+    //     }
+    //     return l;
+    // }
+
+    //Morris Traversal Solution(T.C-O(N) S.C-O(1))
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer>l=new ArrayList<>();
-        Stack<TreeNode>st=new Stack<>();
-        TreeNode temp=root;
-        while(true){
-            if(temp!=null){
-                st.push(temp);
-                temp=temp.left;
+        TreeNode cur=root;
+        while(cur!=null){
+            if(cur.left==null){
+                l.add(cur.val);
+                cur=cur.right;
             }
             else{
-                if(st.isEmpty()) break;
-                temp=st.pop();
-                l.add(temp.val);
-                temp=temp.right;
+                TreeNode pred=PredecessorFind(cur);
+                if(pred.right==null){
+                    pred.right=cur;
+                    cur=cur.left;
+                }
+                else{
+                    pred.right=null;
+                    l.add(cur.val);
+                    cur=cur.right;
+                }
             }
         }
         return l;
     }
+
+    public TreeNode PredecessorFind(TreeNode cur){
+        TreeNode pred=cur.left;
+        while(pred.right!=null && pred.right!=cur){
+            pred=pred.right;
+        }
+        return pred;
+    }
+
 }
