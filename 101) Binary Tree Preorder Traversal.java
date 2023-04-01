ca49944da573f290f9d3bf3318295a8b2d3094aa
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-04-01 12:43:49 +0530

    Update 101) Binary Tree Preorder Traversal.java

--- 101) Binary Tree Preorder Traversal.java
+++ 101) Binary Tree Preorder Traversal.java
@@ -33,26 +33,61 @@ class Solution {
     // }
 
     //iterative
-    public List<Integer> preorderTraversal(TreeNode root) {
-        Stack<TreeNode> st=new Stack<>();
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     Stack<TreeNode> st=new Stack<>();
         
-        List<Integer>ans=new ArrayList<>();
-        if(root==null) return ans;
+    //     List<Integer>ans=new ArrayList<>();
+    //     if(root==null) return ans;
 
-        st.push(root);
-        while(!st.isEmpty()){
-            TreeNode top=st.peek();
-            ans.add(top.val);
-            st.pop();
+    //     st.push(root);
+    //     while(!st.isEmpty()){
+    //         TreeNode top=st.peek();
+    //         ans.add(top.val);
+    //         st.pop();
             
-            if(top.right!=null){
-                st.push(top.right);
+    //         if(top.right!=null){
+    //             st.push(top.right);
+    //         }
+    //         if(top.left!=null){
+    //             st.push(top.left);
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+
+    //Morris Traversal(T.C- O(N) S.C-O(1))
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer>l=new ArrayList<>();
+        TreeNode cur=root;
+        while(cur!=null){
+            if(cur.left==null){
+                l.add(cur.val);
+                cur=cur.right;
             }
-            if(top.left!=null){
-                st.push(top.left);
+            else{
+                TreeNode pred=Predecessor(cur);
+                if(pred.right==null){
+                    pred.right=cur;
+                    l.add(cur.val);
+                    cur=cur.left;
+                }
+                else{
+                    pred.right=null;
+                    cur=cur.right;
+                }
             }
         }
-        return ans;
+        return l;
     }
+
+    public TreeNode Predecessor(TreeNode cur){
+        TreeNode temp=cur.left;
+        while(temp.right!=null && temp.right!=cur){
+            temp=temp.right;
+        }
+        return temp;
+    }
+
 
 }
