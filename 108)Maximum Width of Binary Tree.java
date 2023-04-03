b2554526b4aa361a9cb67f0968b114ee23ec0b2e
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-04-03 11:47:49 +0530

    Update 108)Maximum Width of Binary Tree.java

--- 108)Maximum Width of Binary Tree.java
+++ 108)Maximum Width of Binary Tree.java
@@ -48,3 +48,48 @@ class Solution {
         return max;
     }
 }
+
+
+//CODE STUDIO
+import java.util.* ;
+import java.io.*; 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	   }
+	}
+
+
+************************************************************/
+public class Solution {
+	public static int getMaxWidth(TreeNode root) {
+		if(root == null) return 0;
+        int ans = 0;
+        Queue<TreeNode> q = new LinkedList<>(); 
+        q.offer(root); 
+        while(!q.isEmpty()){
+            int size = q.size();
+			ans = Math.max(ans, size);
+			for(int i=0;i<size;i++){
+				TreeNode node=q.poll();
+					
+				if(node.left != null)
+					q.offer(node.left);
+				if(node.right != null) 
+					q.offer(node.right);
+			}
+            
+        }
+        return ans;
+	}
+}
