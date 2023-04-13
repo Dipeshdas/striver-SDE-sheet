8dc38778b3e3fad96e7af314ba2511d66949e7bc
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-04-13 18:52:01 +0530

    Create 129)Lowest Common Ancestor of a Binary Search Tree.java

---
+++ 129)Lowest Common Ancestor of a Binary Search Tree.java
@@ -0,0 +1,70 @@
+//LEETCODE 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+
+        if(root==p || root==q) return root;
+
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+
+        if(left==null) return right;
+        if(right==null) return left;
+
+        return root;
+
+    }
+}
+
+//CODE STUDIO
+import java.util.* ;
+import java.io.*; 
+/*
+
+    Following is the TreeNode class structure
+
+    class TreeNode<T> 
+    {
+       public:
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        TreeNode(T data) 
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    };
+
+*/
+
+public class Solution 
+{
+
+	public static TreeNode<Integer> LCAinaBST(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) 
+    {		
+        if(root==null) return null;
+
+        if(root==p || root==q) return root;
+
+        TreeNode<Integer> left=LCAinaBST(root.left,p,q);
+        TreeNode<Integer> right=LCAinaBST(root.right,p,q);
+
+        if(left==null) return right;
+        if(right==null) return left;
+
+        return root;
+	}
+}
