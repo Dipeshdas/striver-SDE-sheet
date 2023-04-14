bd49235477766ce6206032bdd51af2b6dfec89aa
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-04-14 17:12:56 +0530

    Create 132)Ceil from BST.java

---
+++ 132)Ceil from BST.java
@@ -0,0 +1,45 @@
+import java.util.* ;
+import java.io.*; 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    class TreeNode<T>
+    {
+       public:
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        TreeNode(T data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    };
+
+************************************************************/
+
+public class Solution {
+
+    public  static int findCeil(TreeNode<Integer> node, int x) {
+
+        int ceil=-1;
+        while(node!=null){
+            if(node.data==x){
+                return node.data;
+            }
+
+            if(x>node.data){
+                node=node.right;
+            }
+            else{
+                ceil=node.data;
+                node=node.left;
+            }
+        }
+        return ceil;
+
+    }
+}
